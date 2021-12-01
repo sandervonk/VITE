b8ecec6ecd1bc3d4ff7e03c9ba0a892c530f46b5
Sander Vonk <sander.c.vonk@gmail.com>
2021-12-01 10:35:01 -0800

    tweaks

--- desktop/js/desktop-menu.js
+++ desktop/js/desktop-menu.js
@@ -515,7 +515,7 @@ $("#verb-custom-share").on("contextmenu", function () {
 $("#answer-input").on("input", function () {
   if ($("#answer-input").val().toLowerCase().includes("rick"))
     $("#rick").html(
-      `<iframe width='100%' height='100%' src='https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&amp;autoplay=1' title='YouTube video player' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen=''></iframe>`
+      `<iframe width='100%' height='100%' src='https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&loop=1&amp;autoplay=1' title='Rick Says Hi :)' frameborder='0' allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen=''></iframe>`
     );
 });
 $(document).on("keydown", function (e) {
