836a773a2324982bbbeab6277007e681cdd38674
Sander Vonk <sander.c.vonk@gmail.com>
2022-05-07 12:20:26 -0700

    work on messaging, remove for release

--- new/serviceWorker.js
+++ new/serviceWorker.js
@@ -215,22 +215,4 @@ self.addEventListener("notificationclick", function (event) {
   event.notification.close();
   event.waitUntil(self.clients.openWindow(event.notification.data));
 });
-function setupMessaging() {
-  firebase.messaging().setBackgroundMessageHandler(function (payload) {
-    console.log(
-      "[firebase-messaging-sw.js] Received background message ",
-      payload
-    );
-    // Customize notification here
-    const notificationTitle = "Background Message Title";
-    const notificationOptions = {
-      body: "Background Message body.",
-      icon: "/logo.png",
-    };
-
-    return self.registration.showNotification(
-      notificationTitle,
-      notificationOptions
-    );
-  });
-}
+//importScripts("firebase-messaging-sw.js");
