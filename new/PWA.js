1e785c5ebab654ce93414b9bf8baf21ccfbabbd3
Sander Vonk <sander.c.vonk@gmail.com>
2022-05-07 12:20:26 -0700

    work on messaging, remove for release

--- new/PWA.js
+++ new/PWA.js
@@ -4,28 +4,7 @@ if ("serviceWorker" in navigator) {
       .register("/VITE/new/serviceWorker.js")
       .then((res) => {
         console.log("service worker registered");
-        try {
-          firebase.messaging().useServiceWorker(res);
-          firebase.messaging().setBackgroundMessageHandler(function (payload) {
-            console.log(
-              "[firebase-messaging-sw.js] Received background message ",
-              payload
-            );
-            // Customize notification here
-            const notificationTitle = "Background Message Title";
-            const notificationOptions = {
-              body: "Background Message body.",
-              icon: "/icon-128x128.png",
-            };
-
-            return self.registration.showNotification(
-              notificationTitle,
-              notificationOptions
-            );
-          });
-        } catch (err) {
-          console.error("firebase messsaging err: ", err);
-        }
+        //firebase.messaging().useServiceWorker(res);
       })
       .catch((err) => console.log("service worker not registered", err));
   });
