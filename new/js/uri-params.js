d9b6cc63d9492c6736351f36aeea01e3c5fb6994
Sander Vonk <sander.c.vonk@gmail.com>
2022-05-11 14:56:46 -0700

    Build actual Sheetify functionality & bugfixes
    - Sheetify tweaks and features
    - Redirects and more for flow pages

---
+++ new/js/uri-params.js
@@ -0,0 +1,9 @@
+let params = new URLSearchParams(window.location.search);
+history.replaceState(
+  {},
+  "",
+  window.location.href.substr(
+    0,
+    window.location.href.length - window.location.search.length
+  )
+);
