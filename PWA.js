0b61557f3ed294047a76e490c25d8e816c48a67f
Sander Vonk <sander.c.vonk@gmail.com>
2022-07-23 21:15:41 -0700

    PWA and Long press
    block pwa on localhost (testing) and import script for iOS 13+ contextmenu

--- PWA.js
+++ PWA.js
@@ -1,4 +1,4 @@
-if ("serviceWorker" in navigator) {
+if ("serviceWorker" in navigator && !(location.hostname === "localhost" || location.hostname === "127.0.0.1")) {
   window.addEventListener("load", function () {
     navigator.serviceWorker
       .register("/VITE/serviceWorker.js")
