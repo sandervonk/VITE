b57267ffb6523351bad1fbc995b370c472b3dc83
Sander Vonk <sander.c.vonk@gmail.com>
2022-08-06 13:23:35 -0700

    start alternate 404

---
+++ 404/404.js
@@ -0,0 +1,30 @@
+// stars
+let limit = window.matchMedia("(orientation: landscape)").matches ? 150 : 75,
+  body = document.body;
+(loop = {
+  start: function () {
+    for (var t = 0; t <= limit; t++) {
+      var n = this.newStar();
+      (n.style.top = 100 * this.rand() + "%"), (n.style.left = 100 * this.rand() + "%"), (n.style.webkitAnimationDelay = this.rand() + "s"), (n.style.mozAnimationDelay = this.rand() + "s"), $("#bg-stars").append(n);
+    }
+  },
+  rand: function () {
+    return Math.random();
+  },
+  newStar: function () {
+    var t = document.createElement("div");
+    return (t.innerHTML = '<figure class="star"></figure>'), t.firstChild;
+  },
+}),
+  loop.start();
+// parallax
+$("#mascot").plaxify({ xRange: 5, yRange: 5 });
+$("#earth-clouds").plaxify({ xRange: 10, yRange: 10 });
+$("#earth-illustration").plaxify({ xRange: 20, yRange: 20, invert: true });
+$("#bg-stars").plaxify({ xRange: 50, yRange: 50, invert: true });
+$.plax.enable();
+$(window).on("resize", function () {
+  $.plax.disable({ restorePositions: true, clearlayers: true });
+  $("#mascot, #earth-clouds, #earth-illustration, #bg-stars").css({ top: "", left: "", transform: "" });
+  $.plax.enable();
+});
