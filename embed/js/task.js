e2eea2b053ec904a718da5c2011345c38f1c590c
Sander Vonk <sander.c.vonk@gmail.com>
2022-10-07 23:42:03 -0700

    start building iframe!

--- embed/js/task.js
+++ embed/js/task.js
@@ -29,35 +29,53 @@ auth.onAuthStateChanged(function (user) {
   console.log("auth state changed");
   if (user) {
     $("[data-role='authwall']").hide();
-    $("[data-role='authcontent']").show();
+    setupContent();
   } else {
     $("[data-role='authwall']").show();
     $("[data-role='authcontent']").hide();
   }
 });
-//*STARS
-let limit = 25,
-  body = document.body;
-loop = {
-  start: function () {
-    for (var i = 0; i <= limit; i++) {
-      var star = this.newStar();
-      star.style.top = this.rand() * 100 + "%";
-      star.style.left = this.rand() * 100 + "%";
-      star.style.webkitAnimationDelay = this.rand() + "s";
-      star.style.mozAnimationDelay = this.rand() + "s";
-      document.getElementById("bg-stars").appendChild(star);
-    }
-  },
-  //to get random number
-  rand: function () {
-    return Math.random();
-  },
-  //creating html dom for star
-  newStar: function () {
-    var d = document.createElement("div");
-    d.innerHTML = '<figure class="star"></figure>';
-    return d.firstChild;
-  },
-};
-loop.start();
+// setup content from userdoc
+function setupContent() {
+  $("#user-image").attr("src", auth.currentUser.photoURL);
+  $("#user-name").text(auth.currentUser.displayName);
+  userDoc()
+    .get()
+    .then(function (doc) {
+      let data = doc.data();
+      if (data) {
+        console.log(data);
+        $("[data-role='authcontent']").show();
+      } else {
+        window.location.reload();
+      }
+    });
+}
+try {
+  //*STARS
+  let limit = 25,
+    body = document.body;
+  loop = {
+    start: function () {
+      for (var i = 0; i <= limit; i++) {
+        var star = this.newStar();
+        star.style.top = this.rand() * 100 + "%";
+        star.style.left = this.rand() * 100 + "%";
+        star.style.webkitAnimationDelay = this.rand() + "s";
+        star.style.mozAnimationDelay = this.rand() + "s";
+        document.getElementById("bg-stars").appendChild(star);
+      }
+    },
+    //to get random number
+    rand: function () {
+      return Math.random();
+    },
+    //creating html dom for star
+    newStar: function () {
+      var d = document.createElement("div");
+      d.innerHTML = '<figure class="star"></figure>';
+      return d.firstChild;
+    },
+  };
+  loop.start();
+} catch (err) {}
