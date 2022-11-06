ae0dc5d59b04ea0d02fc7064c7a60d4bb75d7c0c
sandervonk <sandervonk@users.noreply.github.com>
2022-11-06 06:09:57 +0000

    Sync & minify 'v0.3.4.5' from main

--- PWA.js
+++ PWA.js
@@ -1,10 +1,1 @@
-"use strict";
-const IS_TESTING = location.hostname === "localhost" || location.hostname === "127.0.0.1";
-if ("serviceWorker" in navigator && !IS_TESTING) {
-  navigator.serviceWorker
-    .register("/VITE/service-worker.js")
-    .then((res) => {
-      console.log("service worker registered:", res);
-    })
-    .catch((err) => console.warn("Service worker not registered w/ error:", err));
-}
+"use strict";const IS_TESTING="localhost"===location.hostname||"127.0.0.1"===location.hostname;"serviceWorker"in navigator&&!IS_TESTING&&navigator.serviceWorker.register("/VITE/service-worker.js").then((e=>{console.log("service worker registered:",e)})).catch((e=>console.warn("Service worker not registered w/ error:",e)));
